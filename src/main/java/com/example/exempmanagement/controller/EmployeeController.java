d71cd43b7346f2825544a5da38e4cce62e5b97fc
Yuto Urashima <yuto.urashima24@gmail.com>
2023-07-30 10:28:06 +0900

    create EmployeeController.java

---
+++ src/main/java/com/example/exempmanagement/controller/EmployeeController.java
@@ -0,0 +1,33 @@
+package com.example.exempmanagement.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.exempmanagement.domain.Employee;
+import com.example.exempmanagement.service.EmployeeService;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+    
+    @Autowired
+    private EmployeeService employeeService;
+
+    /**
+     * 従業員リストを取得し、画面に表示
+     * @param model 画面にデータを渡すために使用
+     * @return 従業員一覧画面
+     */
+    @GetMapping("showList")
+    public String showList(Model model) {
+        List<Employee> employeeList = employeeService.showList();
+        model.addAttribute("employeeList", employeeList);
+        return "employee/list";
+    }
+    
+}
